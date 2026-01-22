bd47ede9446bc6c0c20d444227681cb7d6a311ec
Meam-20 <fahmidameam22@gmail.com>
2026-01-22 22:04:05 +0600

    firebase connection

---
+++ frontend/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4mq_0oir34-xyoUazgdtLU39k3PQabSc",
+  authDomain: "meam123-dd0c2.firebaseapp.com",
+  projectId: "meam123-dd0c2",
+  storageBucket: "meam123-dd0c2.firebasestorage.app",
+  messagingSenderId: "746961716799",
+  appId: "1:746961716799:web:95b70e0df7530c0892da8a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
